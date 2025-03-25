57ad2b1463fc77e927d45f5836f7bf44f54bdd4b
TodorChankov <TodorChankov@students.softuni.bg>
2025-03-25 20:40:42 +0200

    Added ProjectService

---
+++ client/src/services/ProjectService.js
@@ -0,0 +1,39 @@
+const baseUrl = 'http://localhost:3030/jsonstore/projects';
+
+export default {
+    async getAll() {
+        const response = await fetch(baseUrl);
+        const projects = await response.json();
+        return Object.values(projects);
+    },
+
+    async getById(id) {
+        const response = await fetch(`${baseUrl}/${id}`);
+        return await response.json();
+    },
+
+    async create(project) {
+        const response = await fetch(baseUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(project)
+        });
+        return await response.json();
+    },
+
+    async update(id, project) {
+        const response = await fetch(`${baseUrl}/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(project)
+        });
+        return await response.json();
+    },
+    
+    async delete(id) {
+        const response = await fetch(`${baseUrl}/${id}`, {
+            method: 'DELETE'
+        });
+        return await response.json();
+    }
+};
