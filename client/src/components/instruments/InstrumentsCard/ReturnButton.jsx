0d429a566e3b808dbf9aa3c8433a27959a1e033d
TodorChankov <TodorChankov@students.softuni.bg>
2025-04-02 10:51:45 +0300

    Return button now works

--- client/src/components/instruments/InstrumentsCard/ReturnButton.jsx
+++ client/src/components/instruments/InstrumentsCard/ReturnButton.jsx
@@ -1,15 +1,17 @@
 import { Button } from "antd";
 
-import EmployeeService from "../../../services/employeeService";
+import { useReturnInstrumentFromEmployee } from "../../api/instrumentsApi";
 
 export default function ReturnButton({ instrument, owner, onReturn }) {
+  const { returnInstrumentFromEmployee } = useReturnInstrumentFromEmployee(); 
+
   return (
     <Button
       type="primary"
       onClick={() => {
         if (instrument.currentOwner) {
           if (owner && owner._id) {
-            EmployeeService.returnInstrumentFromEmployee(owner._id, instrument._id).then(() => {
+            returnInstrumentFromEmployee(owner._id, instrument._id).then(() => {
               onReturn(); // Refresh the instruments list
             });
           } else {
